c495bfde5724056f93047b9348b62a4c0c5440f6
Tony Burge <tonymburge@hotmail.com>
2020-09-15 18:30:15 -0400

    completed deployed/hosted testing for all permissions. now want to cleann up unit tests and config file

--- test_app_hosted.py
+++ test_app_hosted.py
@@ -3,8 +3,7 @@
 # @file name: test_app_hosted.py
 # @purpose: test file of backend app on hosted heroku instance
 # @out_of_scope: no need to test every possible error. that is conducted
-# in the unit tests. Mostly testing permissions here with a few error
-# conditions added
+# in the unit tests of test_app_unit_local.py
 # @author: Tony Burge
 # @date: 2020-09-12
 #
@@ -35,19 +34,66 @@ def print_greeting():
     print("Submitted by Tony Burge")
     print("#" * 80)
 
-# delete all entries. this will clear the N:M table since requests
-# are ran through deployed python using SQL Alchemy ORM
-def clean_database(acct):
+# This function is called to set database to a known state
+# Only executive producer role is used. If an error is encountered,
+# likely due to expired JWT
+def set_db_to_known_state():
     #
+    # find executive producer JWT
+    print("...finding Executive Producer account...", end='')
+    for acct in ACCTS:
+        if acct['type'] == "Executive Producer":
+            break
+    print("Success")
+    #
+    # set up header with permissions of executive producer
     header = {
         "Authorization": f"Bearer {acct['jwt']}",
         "Content-Type": "application/json"
     }
+    #
+    # delete all movies and actors. N:M relationships will be removed based
+    # on SQLAlechemy ORM model implemented in models.py/app.py
     for resource in ["movies", "actors"]:
+        print("...deleting all " + resource + "...", end='')
         resource_list = get_resource(acct, resource)
         for item in resource_list:
             res = (requests.delete(URL + f"{resource}/" + str(item['id']),
                    headers=header))
+    #
+    # add actors
+    print("...adding actors...")
+    for actor in ACTORS:
+        payload = ('{"name": ' + '"' + actor[0] + '"' + ', "dob": ' + '"' +
+                   actor[1] + '"' + ', "gender": ' + '"' + actor[2] + '"}')
+        add_resource(acct, "actors", payload)
+    #
+    # add movies
+    # must first get list of actors to add to each movie
+    print("...adding movies...")
+    actor_list = get_resource(acct, "actors")
+    for movie in MOVIES:
+        payload = ('{"title": ' + '"' + movie[0] + '"' +
+                   ', "release_date": ' + '"' + movie[1] + '"' +
+                   ', "cast": ' + '[')
+        # choose random number to determine how many actors to add to movie
+        # to match actor_list indeces, had to modify with -1 on len
+        # then add one to represent a number from 1 to actors
+        cast_num = randint(0, len(actor_list) - 1) + 1
+        for x in range (0, cast_num):
+            payload += str(actor_list[x]['id'])
+            if x != cast_num - 1:
+                payload += ", "
+        payload += "]}"
+        add_resource(acct, "movies", payload)
+    #
+    # display actors and movies
+    print("...resulting database:")
+    print("\n* Actors *")
+    print(get_resource(acct, "actors"))
+    print("\n* Movies *")
+    print(get_resource(acct, "movies"))
+    print("\n* Database Ready *\n\n")
 
 # function to get actor or movie list based on "resource" argument passed
 def get_resource(acct, resource):
@@ -60,8 +106,10 @@ def get_resource(acct, resource):
     res = requests.get(url, headers=header)
     if res.json()['success']:
         resource_list = res.json()[f'{resource}']
+        print("Success")
     else:
         resource_list = []
+        print("Failed: " + res.json()['message'])
     return resource_list
 
 # function to add new actor or movie based on "resource" agrument passed
@@ -72,9 +120,12 @@ def add_resource(acct, resource, payload):
       "Authorization": f"Bearer {acct['jwt']}",
       "Content-Type": "application/json"
     }
+    print("......adding: " + payload + "...", end='')
     res = requests.post(url, data=payload, headers=header)
-    print("...attempting to add: " + payload + "...", end ='')
-    print("with results:", res.json()['success'])
+    if res.json()['success']:
+        print("Success" )
+    else:
+        print("Failed: " + res.json()['message'])
 
 # function to update actor or movie based on "resource" argument based
 def update_resource(acct, resource, payload, id):
@@ -85,134 +136,133 @@ def update_resource(acct, resource, payload, id):
       "Content-Type": "application/json"
     }
     res = requests.patch(url, data=payload, headers=header)
+    if res.json()['success']:
+        print("Success" )
+    else:
+        print("Failed: " + res.json()['message'])
 
-# main app
+# function to delete actor or movie based on id
+def delete_resource(acct, resource, id):
+    #
+    url = URL + resource + "/" + str(id)
+    header = {
+      "Authorization": f"Bearer {acct['jwt']}",
+      "Content-Type": "application/json"
+    }    
+    res = requests.delete(url, headers=header)
+    if res.json()['success']:
+        print("Success" )
+    else:
+        print("Failed: " + res.json()['message'])
+
+##########################################################################
+# MAIN APP
+##########################################################################
 #
+# display greeting and set database to known state
 print_greeting()
+print("\n* Setting database to known state at start of test *")
+set_db_to_known_state()
 #
-# traverse each account type and perform hosted tests
+# ------------------------------------------------------------------------
+# GET ACTORS and MOVIE TESTS
+# ------------------------------------------------------------------------
+print("----------------------------------------------------------------")
+print("GET tests for each account type")
 for acct in ACCTS:
-    #
-    print("\n" * 2)
-    print("--------------------------------------------------------------")
-    print(f">>> Testing: {acct['type']}")
-    #
-    # clean database with executive producer each time. This is to ensure
-    # data is all clear before testing. Should work unless JWT for exec
-    # producer has expired or if records do not exist
-    print("\n* Cleaning databased using executive producer for next role test")
-    clean_database(acct)
-    #
-    # -----------------------------------------------------------------------
-    # add actors - properly formatted, should work unless user does not have
-    # permission or if JWTs have expried
-    print("\n* Adding actors: should pass if use has permission *")   
-    # for actor in ACTORS:
-    #     add_actor(acct, actor)
-    payload = ""
-    for actor in ACTORS:
-        payload = ('{"name": ' + '"' + actor[0] + '"' + ', "dob": ' + '"' +
-                   actor[1] + '"' + ', "gender": ' + '"' + actor[2] + '"}')
-        add_resource(acct, "actors", payload)
-    #
-    # add actor with missing data. this should fail even if user
-    # has permission to add actor
-    print("\nMissing data. Should fail regardless of permission.")
-    payload = '{"name": "Jonny Depp"}'
+    print("\n" + acct['type'])
+    print("...get actors...", end='')
+    get_resource(acct, "actors")
+    print("...get movies...", end='')
+    get_resource(acct, "actors")
+
+# ------------------------------------------------------------------------
+# POST / ADD ACTOR and MOVIE TESTS
+# ------------------------------------------------------------------------
+print("\n----------------------------------------------------------------")
+print("POST tests to add actor for each account type")
+for acct in ACCTS:
+    print("\n" + acct['type'])
+    print("...add actor...", end='')
+    # can use same actor info. duplicates allowed.
+    payload = ('{"name": "AddTest Actor", "dob": "1/1/2000", "gender": "F"}')
     add_resource(acct, "actors", payload)
-
+#
+print("\n----------------------------------------------------------------")
+print("POST tests to add movie for each account type")
+for acct in ACCTS:
+    print("\n" + acct['type'])
     #
-    # -----------------------------------------------------------------------
-    # add movies - properly formatted, should work unless user does not have
-    # permission or if JWTs have expried
-    # first must find ID of actors
-    print("\n* Adding movies: should pass if user has permission *")       
-    actor_list = get_resource(acct, "actors")
-    if len(actor_list) == 0:
-        print("No actors available to add movies. Check permissions " +
-              "when adding actors.")
-    else:
-        for movie in MOVIES:
-            payload = ('{"title": ' + '"' + movie[0] + '"' +
-                       ', "release_date": ' + '"' + movie[1] + '"' +
-                       ', "cast": ' + '[')
-            # choose random number to determine how many actors to add to movie
-            # to match actor_list indeces, had to modify with -1 on len
-            # then add one to represent a number from 1 to actors
-            cast_num = randint(0, len(actor_list) - 1) + 1
-            for x in range (0, cast_num):
-                payload += str(actor_list[x]['id'])
-                if x != cast_num - 1:
-                    payload += ", "
-            payload += "]}"
-            add_resource(acct, "movies", payload)
-    #
-    # add movie with missing field(s). this should fail regardless of
-    # permissions
-    print("\nMissing data. Should fail regardless of permission.")
-    payload = '{"title": "Raising Arizona"}'
-    add_resource(acct, "actors", payload)   
-    #
-    # add movie with empty cast list. this should fail regardless of
-    # permissions
-    print("\nEmpty cast list. Should fail regardless of permission.")
-    payload = ('{"title": "Raising Arizona", "release_date":' +
-               ' "01/01/2000", "cast": []}')
-    add_resource(acct, "actors", payload)   
-    #
-    # -----------------------------------------------------------------------
-    # update actor
-    print("\n* Updating actor record: should pass if user has permission *")       
-    actor_list = get_resource(acct, "actors")
-    if len(actor_list) == 0:
-        print("No actors available to update. Check permissions " +
-              "when adding actors.")
-    # update first record available
-    else:
-        actor = actor_list[0]
-        payload = '{"name": "UpdateTest Actor"}'
-        print("...attempting to update", actor)
-        print("...with " + payload)
-        update_id = actor['id']
-        update_resource(acct, "actors", payload, update_id)
-        #
-        # get new actor list to verify
+    # must get actor ids here using role with permission. actor ids can't be
+    # random as enpoint API checks for valid actor ids
+    if acct['type'] == "Executive Producer":
         actor_list = get_resource(acct, "actors")
-        for actor in actor_list:
-            if actor['id'] == update_id:
-                print("...updated: ", actor)
-                break
-    #
-    # -----------------------------------------------------------------------
-    # update movie
-    print("\n* Updating movie record: should pass if user has permission *")       
-    movie_list = get_resource(acct, "movies")
-    if len(movie_list) == 0:
-        print("No movies available to update. Check permissions " +
-              "when adding movies.")
-    # update first record available
-    else:
-        movie = movie_list[0]
-        payload = '{"title": "The Updated Movie"}'
-        print("...attempting to update", movie)
-        print("...with " + payload)
-        update_id = movie['id']
-        update_resource(acct, "movies", payload, update_id)
-        #
-        # get new movie list to verify
-        movie_list = get_resource(acct, "movies")
-        for movie in movie_list:
-            if movie['id'] == update_id:
-                print("...updated: ", movie)
-                break
+        actor_ids = ([actor_list[0]['id'], actor_list[1]['id'],
+                     actor_list[2]['id']])
+    print("...add movie...", end='')
+    # can use same movie info. duplicates allowed.
+    payload = ('{"title": "AddTest Movie", "release_date": "2/2/2002", "cast"')
+    payload += f': {actor_ids}' + '}'
+    add_resource(acct, "movies", payload)
 
+# ------------------------------------------------------------------------
+# PATCH / UPDATE ACTOR and MOVIE TESTS
+# ------------------------------------------------------------------------
+#
+# must get actor and movie ids using account with known permissions
+# as endpoing APIs check for valid ids before performing this action
+actor_list = get_resource(ACCTS[0], "actors")
+movie_list = get_resource(ACCTS[0], "movies")
+#
+print("\n----------------------------------------------------------------")
+print("PATCH tests to update an actor record for each account type")
+for acct in ACCTS:
+    print("\n" + acct['type'])
+    actor_to_update = actor_list[0]['id']
+    print("...update actor...", end='')
+    # can update with same info. no checks on if update info is same
+    # as existing
+    payload = '{"name": "UpdateTest Actor"}'
+    update_resource(acct, "actors", payload, actor_to_update)
+#
+print("\n----------------------------------------------------------------")
+print("PATCH tests to update a movie record for each account type")
+for acct in ACCTS:
+    print("\n" + acct['type'])
+    movie_to_update = movie_list[0]['id']
+    print("...update movie...", end='')
+    # can update with same info. no checks on if update info is same
+    # as existing
+    payload = '{"title": "UpdateTest Movie"}'
+    update_resource(acct, "movies", payload, movie_to_update)
 
-    #
-    # -----------------------------------------------------------------------
-    # delete actor
-
-    #
-    # -----------------------------------------------------------------------
-    # delete movie
-
-
+# ------------------------------------------------------------------------
+# DELETE ACTOR and MOVIE TESTS
+# ------------------------------------------------------------------------
+#
+# must get actor and movie ids using account with known permissions
+# as endpoing APIs check for valid ids before performing this action
+actor_list = get_resource(ACCTS[0], "actors")
+movie_list = get_resource(ACCTS[0], "movies")
+#
+print("\n----------------------------------------------------------------")
+print("DELETE tests to delete an actor record for each account type")
+#
+x = 0
+for acct in ACCTS:
+    print("\n" + acct['type'])
+    actor_to_delete = actor_list[x]['id']
+    print("...delete actor...", end='')
+    delete_resource(acct, "actors", actor_to_delete)
+    x += 1  # this is used to index to next actor after previous deleted
+#
+print("\n----------------------------------------------------------------")
+print("DELETE tests to delete a movie record for each account type")
+#
+x = 0
+for acct in ACCTS:
+    print("\n" + acct['type'])
+    movie_to_delete = movie_list[x]['id']
+    print("...delete movie...", end='')
+    delete_resource(acct, "movies", movie_to_delete)
+    x += 1  # this is used to index to next actor after previous deleted
